9d559930aa69b775e81e5b736e43da05b93d2363
kristina.salini <shiriagina@yahoo.com>
2022-10-19 13:54:15 +0200

    add 2 sumbol and delete 1st argument from return of the createPassword method

--- fashionAppi/src/main/java/model/User.java
+++ fashionAppi/src/main/java/model/User.java
@@ -81,7 +81,7 @@ public class User {
 	public String createPassword() {
 
 	
-		return Integer.toString(favoriteNumber%6) + username+symbol+ Integer.toString(additionalNumber);
+		return symbol+symbol+symbol+ username+symbol+ Integer.toString(additionalNumber);
 
     }
 }
